548a1197906b7bda446c9c86cb2af86d17ffbecb
miguelramosalarcon <mramos20681@gmail.com>
2026-02-23 23:13:34 -0500

    Agregar DatePicker y TimePicker al formulario de registro

--- app/src/main/java/com/example/novaserviciosapp/ui/AddServiceActivity.java
+++ app/src/main/java/com/example/novaserviciosapp/ui/AddServiceActivity.java
@@ -5,22 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.novaserviciosapp.R;
 import com.google.android.material.appbar.MaterialToolbar;
 
+//Librerias para DatePicker y TimePicker
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+import java.util.Locale;
+import android.widget.EditText;
+import android.app.TimePickerDialog;
+
 /**
  * AddServiceActivity
  * Pantalla encargada de registrar un nuevo servicio.
  * En esta fase solo muestra la interfaz y permite volver al Dashboard.
  */
+
 public class AddServiceActivity extends AppCompatActivity {
+
+    // ==============================
+    // Variables de interfaz (UI)
+    // ==============================
+    private EditText etFecha;
+    private EditText etHora;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service);
 
+        etFecha = findViewById(R.id.etFecha);
+        etFecha.setOnClickListener(v -> showDatePicker());
+
+        etHora = findViewById(R.id.etHora);
+        etHora.setOnClickListener(v -> showTimePicker());
+
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         toolbar.setNavigationOnClickListener(v -> finish());
+
+
     }
 
     /**
@@ -32,4 +54,69 @@ public class AddServiceActivity extends AppCompatActivity {
         finish();
         return true;
     }
+
+    //Metodo para usar y llamar al DatePicker-calendario
+    private void showDatePicker() {
+        final Calendar calendar = Calendar.getInstance();
+
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+
+                    // Formato ISO (yyyy-MM-dd)
+                    String formattedDate = String.format(
+                            Locale.getDefault(),
+                            "%04d-%02d-%02d",
+                            selectedYear,
+                            selectedMonth + 1,
+                            selectedDay
+                    );
+
+                    etFecha.setText(formattedDate);
+                },
+                year,
+                month,
+                day
+        );
+
+        datePickerDialog.show();
+    }
+
+    //Metodo para usar y llamar al TimePicker
+
+    private void showTimePicker() {
+
+        // Obtiene la hora actual del sistema
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                this,
+                (view, selectedHour, selectedMinute) -> {
+
+                    // Formato HH:mm (24 horas)
+                    String formattedTime = String.format(
+                            Locale.getDefault(),
+                            "%02d:%02d",
+                            selectedHour,
+                            selectedMinute
+                    );
+
+                    etHora.setText(formattedTime);
+                },
+                hour,
+                minute,
+                true // true = formato 24 horas
+        );
+
+        timePickerDialog.show();
+    }
+
+
+
 }

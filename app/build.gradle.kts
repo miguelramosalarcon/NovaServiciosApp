ec7265d8214b42ebdd2e11983cd494de83974695
miguelramosalarcon <mramos20681@gmail.com>
2026-02-28 16:32:57 -0500

    HT04 - Inicio del ht04 configuracion inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+// Convertidor JSON → Java (Gson)
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }

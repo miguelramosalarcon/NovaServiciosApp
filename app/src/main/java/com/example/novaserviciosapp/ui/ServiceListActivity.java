f5bb012bb0b1883c7a44388b57458f25635e869f
miguelramosalarcon <mramos20681@gmail.com>
2026-03-06 10:00:39 -0500

    Mejoramiento de colores en material cards

--- app/src/main/java/com/example/novaserviciosapp/ui/ServiceListActivity.java
+++ app/src/main/java/com/example/novaserviciosapp/ui/ServiceListActivity.java
@@ -15,6 +15,9 @@ import java.util.List;
 import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
+
+
+
 /**
  * ServiceListActivity
  * Muestra el listado de servicios activos usando RecyclerView.
